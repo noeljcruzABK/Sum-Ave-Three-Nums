f78e56d58ab29206dd50aa520e6c0197d6f98523
Noel Cruz <nocruz@activision.com>
2022-07-19 17:25:15 -0400

    added functions to calculate the sum and average of the values entered by the user. Added comments outlining the challenge prompt.

--- Sum-Ave-Three-Nums/Sum-Ave-Three-Nums.cpp
+++ Sum-Ave-Three-Nums/Sum-Ave-Three-Nums.cpp
@@ -1,27 +1,26 @@
-// Sum-Ave-Three-Nums.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Write a program to find the “Sum” and “Average” of “three” numbers.
+// Provide a hint to the user, take the input and return some output results to show the user the calculations for their three numbers.
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int first;
-    int second;
-    int third;
-
-    cout << "Please enter three whole numbers (separated by a space or enter):\n";
-    cin >> first >> second >> third;
-    cout << "You entered " << first << ", " << second << ", and " << third << ".";
+int sumValues(int one, int two, int three) {
+	return one + two + three;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int aveValues(int one, int two, int three) {
+	return (one + two + three) / 3;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+	int first;
+	int second;
+	int third;
+
+	cout << "Please enter three whole numbers (separated by a space or enter):\n";
+	cin >> first >> second >> third;
+	cout << "You entered: " << first << ", " << second << ", and " << third << ".\n";
+	cout << "The sum of your numbers is: " << sumValues(first, second, third) << ".\n";
+	cout << "The average of your numbers is: " << aveValues(first, second, third) << ".\n";
+}
